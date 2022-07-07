39533ce2407745bb4096f68fc335ebbf7135d2c0
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-07 22:45:20 +0530

    Add files via upload

---
+++ input_ways_detail.py
@@ -0,0 +1,79 @@
+# a=input('Enter Your Name:')
+# print("My Name is",a)
+# print("My Name is",a,sep="-")
+# print("My Name is",int(a))
+# print("My Name is",float(a))
+# print("My Name is"+" "+str(a))
+# print("My Name is"+" "+a)
+
+# a=int(input("Enter Your Phone Number:"))
+# print('Your Phone Number is',a)
+# print('Your Phone Number is',a,sep=" ")
+# print('Your Phone Number is',int(a))
+# print('Your Phone Number is',float(a))
+# print('Your Phone Number is'+' '+str(a))
+
+# a=float(input('Enter Your BMI:'))
+# print('Your BMI is',a)
+# print('Your BMI is',a,sep='_')
+# print('Your BMI is',int(a))
+# print('Your BMI is',float(a))
+# print('Your BMI is'+' '+str(a))
+
+# name=input('Enter Your Name:')
+# age=int(input('Enter Your Age:'))
+# per=float(input('Enter Your Graduation Percentge:'))
+# print(name,age,per)
+# print(age,name,per)
+# print(name)
+
+
+# name=input('Enter Your Name:')
+# age=int(input('Enter Your Age:'))
+# per=float(input('Enter Your Graduation Percentage:'))
+
+# print('Your Name is',name,'Your Age is',age,\
+# 'Your Graduation Percentage is',per)
+
+# print('Your Name is',name,'and Age is',age,\
+# 'Graduation Percentage is',per)
+
+# print('Your Name is'+' '+name,'and age is',age,\
+# 'Graduation Percentage is',str(per)+'%')
+
+# print('Your Name is %s and age is %d \
+# Graduation percentage is %.4f'%(name,age,per))
+
+# print('Your Name is {} and age is {} \
+# graduation percentahe is {}'.format(name,age,per))
+
+# print('Your Name is {0} and age is {1} \
+# graduation percentahe is {2}'.format(name,age,per))
+
+# print(f'Your Name is {name} and age is {age} \
+# graduation percentahe is {per}'.format(name,age,per))
+
+# a=int(input())
+# b=int(input())
+# print(a+b)
+
+# a,b=[float(x) for x in input('Enter any two Numbers: ').split()]
+# print(a+b)
+
+# a=[float(x) for x in input().split(',')]
+# print(sum(a))
+
+# import math
+# a=[float(x) for x in input().split(',')]
+# print(math.prod(a))
+
+# x='12345678'
+# print(int(x[0]+int(x[-1])))
+
+
+
+
+
+
+
+
